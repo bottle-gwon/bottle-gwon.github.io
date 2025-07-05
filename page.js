186726a3f41ba87236f3218b05546ce794fa62cb
bottle-gwon <lazyguy1@daum.net>
2025-07-06 03:25:11 +0900

    🔨 Script update
    스크립트 추가

--- page.js
+++ page.js
@@ -1,17 +1,49 @@
 const about = {
-    title: "페이지 소개",
+    title: "<h2>페이지 소개<h2>",
 
     text : "<div> hello </div>"
 }
 
 const htmlCss = {
+    title:"<h2>html+css 실습<h2>",
+
+    text : [
+        `<div class="list-detail-eli">
+                <img src="./PR/PR.png" alt="클로코딩 이미지" class="detail-img">
+                <p class="list-explain">자기소개 제작 페이지<br>2025.05.27<br>자기소개 페이지에 스타일시트를 추가했습니다.</p>
+                <a href="./PR/" class="eli-link">바로가기</a>
+        </div>`,
+
+        `<div class="list-detail-eli">
+                <img src="./layout/layout.png" alt="클로코딩 이미지" class="detail-img">
+                <p class="list-explain">레이아웃 잡아보기<br>2025.05.27<br>flex속성으로 레이아웃을 만들어 봤습니다.</p>
+                
+                <a href="./layout/" class="eli-link">바로가기</a>
+        </div>`,
+
+        `<div class="list-detail-eli">
+                <img src="./clone_coding/clone_coding.png" alt="클로코딩 이미지" class="detail-img">
+                <p class="list-explain">당근 마켓 클론 코딩<br>2025.05.28<br>실습시간에 간단하게 클론 코딩을 진행 해봤습니다.</p>
+                
+                <a href="./clone_coding/" class="eli-link">바로가기</a>
+        </div>`
+    ]
+}
+
+const javaScript = {
+    title:"<h2>자바스크립트 실습<h2>"
 
 }
 
-const javaScript = {}
-
 const study = {
+    title:"<h2>스터디원<h2>",
 
+    text: [
+        "<p>오즈코딩 프론트엔드 12기 깃헙 페이지 제작 스터디원들</p>",
+        "<a href='https://thepott.github.io/'>하흥주님</a>",
+        "<a href='https://kyung-bok.github.io/'>윤경복님</a>",
+        "<a href='https://danbi-seo.github.io/'>서단비님</a>",
+    ]
 }
 
 export { about, htmlCss, javaScript, study };
--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 import { about, study, htmlCss, javaScript } from "./page.js";
 const draw = document.querySelector(".list").children;
+const dashBtn = document.querySelector(".dash-board").children;
+
+const key = {
+    1: about,
+    2: htmlCss,
+    3: javaScript,
+    4: study
+}
 
 console.log(draw)
 
 console.log(about)
 
+console.log(dashBtn[1]);
 
 //처음 실행되면 about 페이지
 window.onload = () =>{
-    draw[0].textContent = about.title;
+    draw[0].innerHTML = about.title;
     draw[1].innerHTML = about.text;
 }
+
+for(let i =1; i< dashBtn.length; i++){
+    dashBtn[i].addEventListener("click", ()=>{
+        draw[0].innerHTML = key[i].title;
+        draw[1].innerHTML = key[i].text;
+    })
+}
+
+// for (const [index, btn] of dashBtn){
+//     if(btn.className === 'dash-board-btn'){
+//         btn.addEventListener("click", ()=>{
+//             draw[0].textContent = key[index].title;
+//             console.log(key[index].title)
+//         })
+//     }
+// }
